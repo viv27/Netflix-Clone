e9acfea025eb350a2eec663c88a19b64a9e71e46
Vivek Mohan <vivekmohan27@gmail.com>
2021-05-09 21:53:50 +0530

    feat: Rendered movie posters on screen

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -5,10 +5,15 @@ import requests from './requests'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Netflix</h1>
-      <Row title="NETFLIX ORIFINALS" fetchURL ={requests.fetchNetflixOriginals}/>
+      <Row isLargeRow title="NETFLIX ORIFINALS" fetchURL ={requests.fetchNetflixOriginals}/>
       <Row title="Trending Now" fetchURL ={requests.fetchTrending}/>
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
 
     </div>
   );
